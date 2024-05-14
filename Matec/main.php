9a0ce1165b1cf92edcd1abc7abeb51a4ea4802ad
Rafa3lFerreira <rftto2007@gmail.com>
2024-05-14 16:11:49 -0300

    feat"começo da nova tela principal"

--- Matec/main.php
+++ Matec/main.php
@@ -18,43 +18,19 @@ session_start();
 </head>
 
 <body>
-    <nav class="navbar bg-light border-bottom border-body" data-bs-theme="light" style="box-shadow: 0 1px 2px 0 rgba(60,64,67,.3), 0 2px 6px 2px rgba(60,64,67,.15);">
-        <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1 navTitulo">MATEC</span>
-
-            <i class="fa-solid fa-circle-user fa-2xl"></i>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="./tema/IMG/matec-preto.png" width="300px">
+            </a>
+            <a href="">
+                <div class="rounded-circle divimg" style="height:50px; width:50px;">
+                    <i class="fa-solid fa-user fa-2xl"></i>
+                </div>
+            </a>
         </div>
     </nav>
-    <div class="container-fluid">
-        <div class="row flex-nowrap">
-            <div class=" col-md-3 col-lg-1 px-sm-2 px-0" style="border-right: 1px solid rgb(218, 220, 224);" >
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu" style="width: 100%; padding:0 10px 0 10px;">
-                        <li class="nav-item esapaco">
-                            <a href="#" class="nav-link align-middle px-0">
-                               ChatBot
-                            </a>
-                        </li>
-                        <li class="nav-item esapaco">
-                            <a href="#" class="nav-link px-0"> 
-                                Listagem Clientes
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <div class="centralizarTitle">
-                    <h2>Bem Vindo Usuario</h2>
-                </div>
-                <div class="centralizarCont">
-                    <div>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
 </body>
 
 </html>
